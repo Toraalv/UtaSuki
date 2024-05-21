b6e84be702a4eb42ead76647b01413983f82f005
Toraalv <toralv_p@proton.me>
2024-05-21 17:18:09 +0200

    version bump

--- api/api.js
+++ api/api.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 const https = require("https");
 const http = require("http");
 // konstanter
